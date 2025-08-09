9a526e435a808a4c407c8273c86b09df5c692b7a
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-08-09 21:58:57 +0300

    feat(add):RootStackNavigator and wrap it in the main AppNavigator

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { useSelector } from 'react-redux';
-import AuthNavigator from './AuthNavigator';
-import AdminTabNavigator from './AdminTabNavigator';
-import LandlordTabNavigator from './LandlordTabNavigator';
-import TenantTabNavigator from './TenantTabNavigator';
-import type { RootState } from '../store/rootReducer';
+import RootStackNavigator from './RootStackNavigator';
 
 export default function AppNavigator() {
-  const { token, role } = useSelector((state: RootState) => state.auth);
-
   return (
     <NavigationContainer>
-      {token == null ? (
-        // No token → show auth screens
-        <AuthNavigator />
-      ) : role === 'admin' ? (
-        // Admin
-        <AdminTabNavigator />
-      ) : role === 'landlord' ? (
-        // Landlord
-        <LandlordTabNavigator />
-      ) : (
-        // Tenant
-        <TenantTabNavigator />
-      )}
+      <RootStackNavigator />
     </NavigationContainer>
   );
 }
